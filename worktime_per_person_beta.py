1caf62cd2cd8e4d103765b96bc399f16afe833c9
jiwon11 <jeewonre@naver.com>
2017-10-08 00:38:31 +0900

    완성

--- worktime_per_person_beta.py
+++ worktime_per_person_beta.py
@@ -13,20 +13,17 @@ class Person:
 
 def set_person(name_list, chn_work_time_list, person_list):
     for i, person_name in enumerate(name_list):
-        if name_list.count(person_name) > 1:
-            name_list.remove(person_name)
         for j, person_work_time in enumerate(chn_work_time_list):
             if i == j:
                 person = Person(person_name, person_work_time)
                 person_list.append(person)
-                for k, person in enumerate(person_list):
-                    if person_list.count(person.name) > 1:
-                        del person_list[k]
-
 
 def print_person(person_list):
-    for person in person_list:
-        person.print_info()
+    try:
+        for person in person_list:
+            person.print_info()
+    except AttributeError as err:
+        print("수직인원 객체가 없습니다...")
 
 
 def add_person():
@@ -60,22 +57,38 @@ def cal_over_worktime(name_list, ave_work_time_list):
 
 
 def store_contact(person_list):
-    f = open("person_db.txt", "wt")
+    f = open("person_db.txt", "wt",encoding='utf8')
     for person in person_list:
         f.write(person.name + '\n')
         f.write(str(person.work_time) + '\n')
     f.close()
 
+def load_person_db(person_list, work_time_list, name_list):
+    f = open("person_db.txt", "rt")
+    lines = f.readlines()
+    num = len(lines) / 2
+    num = int(num)
+
+    for i in range(num):
+        person_name = lines[2*i].rstrip('\n')
+        person_work_time = lines[2*i+1].rstrip('\n')
+        name_list.append(person_name)
+        work_time_list.append(person_work_time)
+        person = Person(person_name, person_work_time)
+        person_list.append(person)
+    f.close()
 
 def run():
     work_time_list = []
     name_list = []
     person_list = []
+    load_person_db(person_list, work_time_list, name_list)
     while 1:
+        print(name_list)
+        print(work_time_list)
         name = add_person()
         name_list.append(name)
         print("수직인원 : ", name_list)
-
         ave_worktime = cal_ave_worktime(name_list)
         work_time_list.clear()
         work_time_list.append(ave_worktime)
@@ -83,20 +96,20 @@ def run():
         print(ave_work_time_list)
         chn_work_time_list = cal_over_worktime(name_list, ave_work_time_list)
         print(chn_work_time_list)
+        person_list.clear()
         set_person(name_list, chn_work_time_list, person_list)
+        print(name_list)
+        print(chn_work_time_list)
         print_person(person_list)
-        store_contact(person_list)
+        if input("DB에 저장하시겠습니까? ") == '네' or 'yes':
+            store_contact(person_list)
+        else:
+            pass
+        print("--------------------------------------------------")
 
 
 if __name__ == '__main__':
     run()
 
-    # 문제점 1
-    # 현재 인원 추가에 따른 수직시간이 바뀔때마다 클래스가 계속 생성됨.--> 이전 클래스를 삭제할 필요 있음.
-    # 이를 해결할 필요성 있음.
-    # 해결법 1
-    # 클래스 생성자 person = Person(person_name, person_work_time)에서 조건문을 통해 이미 클래스가 있을 경우 이전 클래스를
-    # 삭제하고 없을 경우 pass한 후 클래스를 생성
-    # 위 문제와 파일 불러오기만 입력하면 완성됨.
-    # 2017.10.부터 개발 재개할 예정
+    # 완성
     # 깃허브로 공유중
